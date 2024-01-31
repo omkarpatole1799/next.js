c43dea14df3a605b6cf96f111f3066687862665d
Omkar Patole <omkarpatole1799@gmail.com>
2024-01-31 07:52:45 +0530

    Refactoring

--- TODO app/lib/db-sequelize-connect.js
+++ TODO app/lib/db-sequelize-connect.js
@@ -1,21 +1,21 @@
-import Sequelize  from 'sequelize'
-import dotenv from 'dotenv'
+import Sequelize from "sequelize"
+import dotenv from "dotenv"
 dotenv.config()
 
-const sequelize = new Sequelize(process.env.DB, process.env.DB_USER,process.env.DB_PASS, {
+const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASS, {
 	host: process.env.DB_HOST,
-	post: '3306',
-	dialect: 'mysql',
-	dialectModule: require('mysql2'),
-	query: {raw: true}
-}) 
+	post: "3306",
+	dialect: "mysql",
+	dialectModule: require("mysql2"),
+	query: { raw: true }
+})
 
 async function getConnectToDb() {
 	try {
 		await sequelize.authenticate()
-		await sequelize.sync({alter: true})		
+		await sequelize.sync({ alter: true })
 	} catch (error) {
-		console.log(error, 'error in connection')
+		console.log(error, "error in connection")
 	}
 }
 
